05e01079e2dc5e4502176308a146b585ff9c5756
Raghava <chinnu4148@gmail.com>
2024-05-26 14:10:07 +0800

    feat: Implemented NextUI library and created Homepage, Navigation Bar and Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import NavBar from "../components/NavBar";
+const Layout = ({ children }: { children?: ReactNode }) => {
+  return (
+    <div className="w-full">
+      <NavBar />
+      <div className="bg-slate-100 min-h-[calc(100vh-65px)]">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,75 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+} from "@nextui-org/react";
+import { useState } from "react";
+import { FaGraduationCap } from "react-icons/fa6";
+
+export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const menuItems = ["Modules", "Current Tutorials"];
+
+  // Only should see modules and current tutorials after student is logged in
+  return (
+    <Navbar
+      maxWidth="full"
+      className="border-b-1 border-b-gray-200"
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <span className="font-bold text-inherit text-2xl flex items-center">
+            <FaGraduationCap
+              style={{ marginRight: 5, color: "rgb(23 37 84)" }}
+            />
+            <span className="text-blue-950">NUS</span>
+            <span className="text-amber-600">TUTS</span>
+          </span>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Modules
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Current Tutorials
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
