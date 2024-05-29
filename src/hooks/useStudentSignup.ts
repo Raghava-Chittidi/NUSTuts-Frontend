16f787092e44a3db871e6b9b83f957b81e06b487
= <yeohchongyics@gmail.com>
2024-05-29 23:40:46 +0800

    feat: add useStudentSignup hook, add proxy for local backend

---
+++ src/hooks/useStudentSignup.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+
+export const useStudentSignup = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState<boolean | null>(null);
+
+    const signup = async (name: string, email: string, password: string) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await axios.post("/api/auth/signupStudent", {
+            Name: name,
+            Email: email,
+            Password: password
+        });
+
+        if (response.status !== 200) {
+            setIsLoading(false);
+            setError(response.data.Error);
+            return;
+        }
+
+        setIsLoading(false);
+    }
+     return { signup, isLoading, error };
+};
