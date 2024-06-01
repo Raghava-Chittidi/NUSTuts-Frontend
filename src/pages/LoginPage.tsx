84cb077c3c60b5fe8f94cf5bf302838b4d5e026a
Raghava <chinnu4148@gmail.com>
2024-06-01 16:32:37 +0800

    fix: merge conflict

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,91 +1,90 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Button, Checkbox, Input } from '@nextui-org/react';
-import { Eye, EyeOff } from '@geist-ui/react-icons';
-import { useLogin } from '../hooks/useLogin';
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Checkbox, Input } from "@nextui-org/react";
+import { Eye, EyeOff } from "@geist-ui/react-icons";
+import { useLogin } from "../hooks/useLogin";
 
 const LoginPage = ({ userType }: { userType: string }) => {
-    const { login, error, isLoading } = useLogin(userType);
+  const { login, error, isLoading } = useLogin(userType);
 
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    
-        const formData = new FormData(event.currentTarget);
-        const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
-    
-        if (!email || !password) {
-            if (!email) setEmailError(true);
-            if (!password) setPasswordError(true);
-            return;
-        }
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        await login(email, password);
-    };
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
 
+    if (!email || !password) {
+      if (!email) setEmailError(true);
+      if (!password) setPasswordError(true);
+      return;
+    }
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name } = event.target;
+    await login(email, password);
+  };
 
-      if (name === "email") {
-        setEmailError(false);
-      }
-      if (name === "password") {
-        setPasswordError(false);
-      }
-    };
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
 
-    return (
-      <div className="min-h-screen flex">
-        <div className="w-full flex items-center justify-center bg-[url('/auth-bg.jpg')] bg-cover">
-          <div className="flex-1 max-w-md p-6 bg-white rounded-lg shadow-lg">
-            <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
-              {userType} Login
-            </h2>
-            <h5 className="text-sm text-center text-gray-500 mb-4">
-              Welcome back! Please enter your details
-            </h5>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
+    if (name === "email") {
+      setEmailError(false);
+    }
+    if (name === "password") {
+      setPasswordError(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex">
+      <div className="w-full flex items-center justify-center bg-[url('/auth-bg.jpg')] bg-cover">
+        <div className="flex-1 max-w-md p-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
+            {userType} Login
+          </h2>
+          <h5 className="text-sm text-center text-gray-500 mb-4">
+            Welcome back! Please enter your details
+          </h5>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <Input
+                name="email"
+                isRequired
+                type="email"
+                label="Email"
+                className="w-full"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <div className="relative">
                 <Input
-                  name="email"
+                  name="password"
                   isRequired
-                  type="email"
-                  label="Email"
+                  type={toggle ? "text" : "password"}
+                  label="Password"
                   className="w-full"
                   onChange={handleInputChange}
                 />
+                <Button
+                  type="button"
+                  variant="light"
+                  onClick={() => setToggle(!toggle)}
+                  className="absolute top-1/2 right-3 transform -translate-y-1/2"
+                >
+                  {toggle ? <EyeOff /> : <Eye />}
+                </Button>
               </div>
-              <div>
-                <div className="relative">
-                  <Input
-                    name="password"
-                    isRequired
-                    type={toggle ? "text" : "password"}
-                    label="Password"
-                    className="w-full"
-                    onChange={handleInputChange}
-                  />
-                  <Button
-                    type="button"
-                    variant="light"
-                    onClick={() => setToggle(!toggle)}
-                    className="absolute top-1/2 right-3 transform -translate-y-1/2"
-                  >
-                    {toggle ? <EyeOff /> : <Eye />}
-                  </Button>
-                </div>
-                {passwordError && (
-                  <p className="text-xs text-red-500 mt-1">
-                    Password is required
-                  </p>
-                )}
-              </div>
+              {passwordError && (
+                <p className="text-xs text-red-500 mt-1">
+                  Password is required
+                </p>
+              )}
+            </div>
             <div className="flex items-center justify-between">
               <Checkbox value="remember">Remember me</Checkbox>
               <Link to="#" className="text-sm text-purple-600">
@@ -110,35 +109,17 @@ const LoginPage = ({ userType }: { userType: string }) => {
                   Sign up
                 </Link>
               </div>
-              <Button
-                type="submit"
-                color="secondary"
-                variant="solid"
-                className="w-full"
-              >
-                LOGIN
-              </Button>
-              {userType === "Student" && (
-                <div className="flex items-center">
-                  <span className="text-sm">Don't have an account?</span>
-                  <Link
-                    to="/student/signup"
-                    className="text-sm text-purple-600 ml-2"
-                  >
-                    Sign up
-                  </Link>
-                </div>
-              )}
-              {error && 
-                <div className="mt-4 p-2 bg-red-100 text-red-700 border border-red-400 rounded">
-                    <p className="text-sm">{error}</p>
-                </div>
-              }
-            </form>
-          </div>
+            )}
+            {error && (
+              <div className="mt-4 p-2 bg-red-100 text-red-700 border border-red-400 rounded">
+                <p className="text-sm">{error}</p>
+              </div>
+            )}
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/student/StudentSignUpPage.tsx
+++ src/pages/student/StudentSignUpPage.tsx
@@ -1,179 +1,194 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Button, Checkbox, CircularProgress, Input } from '@nextui-org/react';
-import { Eye, EyeOff } from '@geist-ui/react-icons';
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Checkbox, CircularProgress, Input } from "@nextui-org/react";
+import { Eye, EyeOff } from "@geist-ui/react-icons";
 import bgpic from "../../assets/student.jpg";
-import { useStudentSignup } from '../../hooks/useStudentSignup';
-import useSWR from 'swr';
-import axios from 'axios';
-import AsyncSelect from 'react-select/async';
-import Select from 'react-select';
-import { ActionMeta, MultiValue } from 'react-select';
+import { useStudentSignup } from "../../hooks/useStudentSignup";
+import useSWR from "swr";
+import axios from "axios";
+import AsyncSelect from "react-select/async";
+import Select, { createFilter } from "react-select";
+import { ActionMeta, MultiValue } from "react-select";
 import { getCurrentAY } from "../../util/util";
 
 const StudentSignUpPage = () => {
-    const navigate = useNavigate();
-    const { signup, signUpError, isSignUpLoading} = useStudentSignup();
+  const navigate = useNavigate();
+  const { signup, signUpError, isSignUpLoading } = useStudentSignup();
 
+  const [toggle, setToggle] = useState(false);
+  const [message, setMessage] = useState("");
 
-    const [toggle, setToggle] = useState(false);
-    const [message, setMessage] = useState("");
-
-    const [nameError, setNameError] = useState(false);
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
-    const [modules, setModules] = useState<{ value: string; label: string }[]>([]);
-    const [selectedModules, setSelectedModules] = useState<string[]>([]);
-    // // Reference: We are using nusmods API
-    // const { error, data, isLoading } = useSWR(
-    //     `https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`,
-    //     (url: string) => axios.get(url).then((res) => res.data)
-    // );
-    useEffect(() => {
-        const fetchModules = async () => {
-            const moduleListResponse = await axios.get(`https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`);
-            const moduleList = moduleListResponse.data.map((nusModule: any) => nusModule.moduleCode);
-            setModules(moduleList.map((moduleCode: string) => ({ value: moduleCode, label: moduleCode })));
-        };
-        fetchModules();
-    }, []);
-
-    const handleSelectedChange = (newValue: MultiValue<{ value: string; label: string }>, actionMeta: ActionMeta<{ value: string; label: string }>) => {
-        setSelectedModules(newValue.map(option => option.value));
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [modules, setModules] = useState<{ value: string; label: string }[]>(
+    []
+  );
+  const [selectedModules, setSelectedModules] = useState<string[]>([]);
+  // // Reference: We are using nusmods API
+  // const { error, data, isLoading } = useSWR(
+  //     `https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`,
+  //     (url: string) => axios.get(url).then((res) => res.data)
+  // );
+  useEffect(() => {
+    const fetchModules = async () => {
+      const moduleListResponse = await axios.get(
+        `https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`
+      );
+      const moduleList = moduleListResponse.data.map(
+        (nusModule: any) => nusModule.moduleCode
+      );
+      setModules(
+        moduleList.map((moduleCode: string) => ({
+          value: moduleCode,
+          label: moduleCode,
+        }))
+      );
     };
+    fetchModules();
+  }, []);
 
-    // const loadOptions = async (searchValue: string) => {
-    //     const moduleListResponse = await axios.get(`https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`);
-    //     const moduleList = moduleListResponse.data;
-    //     return filterOptions(moduleList, searchValue);
-    // };
+  const handleSelectedChange = (
+    newValue: MultiValue<{ value: string; label: string }>,
+    actionMeta: ActionMeta<{ value: string; label: string }>
+  ) => {
+    setSelectedModules(newValue.map((option) => option.value));
+  };
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    
-        const formData = new FormData(event.currentTarget);
-        const name = formData.get('name') as string;
-        const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
-    
-        if (!name || !email || !password) {
-            if (!name) setNameError(true);
-            if (!email) setEmailError(true);
-            if (!password) setPasswordError(true);
-            return;
-        }
-    
-        await signup(name, email, password, selectedModules);
-    };
+  // const loadOptions = async (searchValue: string) => {
+  //     const moduleListResponse = await axios.get(`https://api.nusmods.com/v2/${getCurrentAY()}/moduleList.json`);
+  //     const moduleList = moduleListResponse.data;
+  //     return filterOptions(moduleList, searchValue);
+  // };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name } = event.target;
-        if (name === 'name') setNameError(false);
-        if (name === 'email') setEmailError(false);
-        if (name === 'password') setPasswordError(false);
-    };
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    return (
-        <div className="min-h-screen flex">
-          <div className="w-full flex items-center justify-center bg-[url('/auth-bg.jpg')] bg-cover">
-            <div className="flex-1 max-w-md p-6 bg-white rounded-lg shadow-lg">
-              <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
-                Student Sign Up
-              </h2>
-              <h5 className="text-sm text-center text-gray-500 mb-4">
-                Join tutorials by signing up as a student.
-              </h5>
-              <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <Input
-                        type="text"
-                        name="name"
-                        label="Full Name"
-                        autoComplete="name"
-                        autoFocus
-                        className="w-full"
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                  <Input
-                    name="email"
-                    isRequired
-                    type="email"
-                    label="Email"
-                    className="w-full"
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div>
-                  <div className="relative">
-                    <Input
-                      name="password"
-                      isRequired
-                      type={toggle ? "text" : "password"}
-                      label="Password"
-                      className="w-full"
-                      onChange={handleInputChange}
-                    />
-                    <Button
-                      type="button"
-                      variant="light"
-                      onClick={() => setToggle(!toggle)}
-                      className="absolute top-1/2 right-3 transform -translate-y-1/2"
-                    >
-                      {toggle ? <EyeOff /> : <Eye />}
-                    </Button>
-                  </div>
-                  {passwordError && (
-                    <p className="text-xs text-red-500 mt-1">
-                      Password is required
-                    </p>
-                  )}
-                </div>
-                {/* <AsyncSelect
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (!name || !email || !password) {
+      if (!name) setNameError(true);
+      if (!email) setEmailError(true);
+      if (!password) setPasswordError(true);
+      return;
+    }
+
+    await signup(name, email, password, selectedModules);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    if (name === "name") setNameError(false);
+    if (name === "email") setEmailError(false);
+    if (name === "password") setPasswordError(false);
+  };
+
+  return (
+    <div className="min-h-screen flex">
+      <div className="w-full flex items-center justify-center bg-[url('/auth-bg.jpg')] bg-cover">
+        <div className="flex-1 max-w-md p-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
+            Student Sign Up
+          </h2>
+          <h5 className="text-sm text-center text-gray-500 mb-4">
+            Join tutorials by signing up as a student.
+          </h5>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <Input
+                type="text"
+                name="name"
+                label="Name"
+                autoComplete="name"
+                autoFocus
+                className="w-full"
+                onChange={handleInputChange}
+                isRequired
+              />
+            </div>
+            <div>
+              <Input
+                name="email"
+                isRequired
+                type="email"
+                label="Email"
+                className="w-full"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <div className="relative">
+                <Input
+                  name="password"
+                  isRequired
+                  type={toggle ? "text" : "password"}
+                  label="Password"
+                  className="w-full"
+                  onChange={handleInputChange}
+                />
+                <Button
+                  type="button"
+                  variant="light"
+                  onClick={() => setToggle(!toggle)}
+                  className="absolute top-1/2 right-3 transform -translate-y-1/2"
+                >
+                  {toggle ? <EyeOff /> : <Eye />}
+                </Button>
+              </div>
+              {passwordError && (
+                <p className="text-xs text-red-500 mt-1">
+                  Password is required
+                </p>
+              )}
+            </div>
+            {/* <AsyncSelect
                     isMulti
                     cacheOptions
                     defaultOptions
                     loadOptions={loadOptions}
                     onChange={handleSelectedChange}
                 /> */}
-                  <Select
-                    defaultValue={[]}
-                    isMulti
-                    name="modules"
-                    options={modules}
-                    className="basic-multi-select"
-                    classNamePrefix="select"
-                    onChange={handleSelectedChange}
-                    placeholder="Select Modules"
-                />
-                <Button
-                  type="submit"
-                  color="secondary"
-                  variant="solid"
-                  className="w-full"
-                >
-                  SIGN UP
-                </Button>
-                <div className="flex items-center">
-                    <span className="text-sm">Already have an account?</span>
-                    <Link
-                        to="/student/login"
-                        className="text-sm text-purple-600 ml-2"
-                    >
-                        Log in
-                    </Link>
-                </div>
-                {signUpError && 
-                  <div className="mt-4 p-2 bg-red-100 text-red-700 border border-red-400 rounded">
-                      <p className="text-sm">{signUpError}</p>
-                  </div>
-                }
-              </form>
+            <Select
+              defaultValue={[]}
+              isMulti
+              name="modules"
+              options={modules}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              onChange={handleSelectedChange}
+              placeholder="Select Modules"
+              required
+            />
+            <Button
+              type="submit"
+              color="secondary"
+              variant="solid"
+              className="w-full"
+            >
+              SIGN UP
+            </Button>
+            <div className="flex items-center">
+              <span className="text-sm">Already have an account?</span>
+              <Link
+                to="/student/login"
+                className="text-sm text-purple-600 ml-2"
+              >
+                Log in
+              </Link>
             </div>
-          </div>
+            {signUpError && (
+              <div className="mt-4 p-2 bg-red-100 text-red-700 border border-red-400 rounded">
+                <p className="text-sm">{signUpError}</p>
+              </div>
+            )}
+          </form>
         </div>
-      );
+      </div>
+    </div>
+  );
 };
 
 export default StudentSignUpPage;
