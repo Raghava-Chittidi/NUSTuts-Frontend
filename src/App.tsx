e1892eb75210d4ff0dc7cfd383c03545e1c6df86
Raghava <chinnu4148@gmail.com>
2024-06-02 00:30:12 +0800

    fix: merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StudentSignUpPage from "./pages/student/StudentSignUpPage";
 import { RequestPage } from "./pages/ta/RequestPage";
 
-// Dummy modules data
-const modules = [
-  {
-    code: "CS1101S",
-    name: "Programming Methodology I",
-  },
-  {
-    code: "MA1522",
-    name: "Linear Algebra",
-  },
-  {
-    code: "MA1521",
-    name: "Calculus",
-  },
-  {
-    code: "IS1128",
-    name: "Programming Methodology I",
-  },
-  {
-    code: "GEA1000",
-    name: "Programming Methodology I",
-  },
-];
-
 /**
  * A browser router containing routes.
  * Add new routes here as needed.
@@ -60,7 +36,7 @@ const router = createBrowserRouter([
   // Render ModulesPage with dummy modules array, to be replaced with real data in future
   {
     path: "/modules",
-    element: <ModulesPage modules={modules} />,
+    element: <ModulesPage />,
   },
   {
     path: "/modules/:moduleCode/tutorials",
--- src/pages/student/ModulesPage.tsx
+++ src/pages/student/ModulesPage.tsx
@@ -1,16 +1,57 @@
+import { useEffect, useState } from "react";
 import ModuleListItem from "../../components/modules/ModuleListItem";
+// import { Module } from "../../types";
+import axios from "axios";
+import { Spinner } from "@nextui-org/react";
+import { getCurrentAY } from "../../util/util";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
-const ModulesPage = ({
-  modules,
-}: {
-  modules: { code: string; name: string }[];
-}) => {
+const ModulesPage = () => {
+  const { state } = useAuthContext();
+  const [mods, setMods] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getMods = async () => {
+      setIsLoading(true);
+      console.log(modules);
+      const promises = [];
+      for (const module of modules!) {
+        promises.push(
+          axios
+            .get(
+              `https://api.nusmods.com/v2/${getCurrentAY()}/modules/${module}.json`
+            )
+            .then((res) => {
+              return {
+                code: res.data.moduleCode,
+                name: res.data.title,
+              };
+            })
+        );
+      }
+
+      const res = await Promise.all(promises);
+      setMods(res);
+      setIsLoading(false);
+    };
+
+    const modules = state.user?.modules;
+    if (modules && modules.length > 0) {
+      getMods();
+    }
+  }, [state.user]);
+
+  if (isLoading || mods.length === 0) {
+    return <Spinner />;
+  }
+
   return (
     <div className="w-full text-center space-y-5 pt-5">
       <h1 className="font-bold text-xl">Current Modules:</h1>
       <div className="flex flex-col w-full items-center space-y-5">
-        {modules.map((module) => {
-          return <ModuleListItem module={module} />;
+        {mods.map((mod, index) => {
+          return <ModuleListItem module={mod} key={index} />;
         })}
       </div>
     </div>
