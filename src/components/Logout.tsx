d289b9af9cef7d8c8128083198633bd6c9918135
= <yeohchongyics@gmail.com>
2024-06-04 23:35:36 +0800

    refactor: lint

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,8 +1,5 @@
 import { Button } from "@nextui-org/button";
 import { useLogout } from "../hooks/useLogout";
-import { useAuthContext } from "../hooks/useAuthContext";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
     const { logout } = useLogout();
