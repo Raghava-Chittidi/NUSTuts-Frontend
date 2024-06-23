16c368712be1d9ffb96a904cf4d0e7db19e20bcf
Raghava <chinnu4148@gmail.com>
2024-06-23 11:52:37 +0800

    refactor: added render api url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://172.17.0.1:8000",
+        target: "https://nustuts-backend.onrender.com",
         changeOrigin: true,
       },
     },
