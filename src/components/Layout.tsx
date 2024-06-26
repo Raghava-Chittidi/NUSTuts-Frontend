384fe57dae6f80198d2f3a132f96b0f6b39605ee
Raghava <chinnu4148@gmail.com>
2024-06-27 07:50:11 +0800

    bug: fixed redirection bug for discussion

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { useEffect } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { isUserStudent } from "../util/user";
 const Layout = () => {
+  const url = useLocation().pathname;
+  const { state, isLoggedIn } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (url === "/" && isLoggedIn) {
+      navigate(isUserStudent(state.user) ? "/modules" : "/requests");
+    }
+  }, []);
+
   return (
     <div className="w-full">
       <NavBar />
